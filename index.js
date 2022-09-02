d180f18349b600c11316364f6bf733677eb045a2
dothinh115 <dothinh115@gmail.com>
2022-09-02 15:35:17 +0700

    nop bai tap them

--- index.js
+++ index.js
@@ -53,9 +53,12 @@ function sapXep() {
 function chaoHoi() {
     //lấy dữ liệu từ input
     var name = document.getElementById("name").value;
-    var hello = "";
+    var hello = "Xin chào ";
     //So sánh dữ liệu với điều kiện
-    if (name == "M") {
+    if (name == "") {
+        hello = "Ai đó?";
+    }
+    else if (name == "M") {
         hello += "Mẹ";
     }
     else if (name == "B") {
@@ -68,7 +71,7 @@ function chaoHoi() {
         hello += "Em gái";
     }
     //Xuất kết quả ra giao diện
-    document.getElementById("result__2").innerHTML = "Xin chào " + hello;
+    document.getElementById("result__2").innerHTML = hello;
 }
 
 //TÌM SỐ CHẴN LẺ
@@ -106,7 +109,10 @@ function timSoChanLe() {
 //XÁC ĐỊNH TAM GIÁC
 function checkTamGiac(a, b ,c) {
     var result = "";
-    if(a == b == c) {
+    if(a <= 0 || b <= 0 || c <=0) {
+        result += "Các cạnh của tam giác không được <= 0";
+    }
+    else if(a == b == c) {
         result += "tam giác đều";
     }
     else if (a == c || a == b || b == c) {
@@ -128,5 +134,215 @@ function xacDinhTamGiac() {
     var c = document.getElementById("canh_3").value * 1;
     var ketQua = checkTamGiac(a, b, c);
     //xuất ra giao diện
-    document.getElementById("result__4").innerHTML = "Đây là " + ketQua;
+    document.getElementById("result__4").innerHTML = ketQua;
+}
+
+// TÍNH NGÀY THÁNG NĂM
+function doanNgayMai (a, b, c) {
+    var result;
+    var ngay;
+    var thang;
+    var nam;
+    if (b == 1 || b == 3 || b == 5 ||b == 7 || b == 8 || b == 10 && a == 31) {
+        ngay = 1;
+        thang = b + 1;
+        nam = c;
+    }
+    else if (b == 12 && a == 31) {
+        ngay = 1;
+        thang = 1;
+        nam = c + 1;
+    }
+    else if (b == 4 || b == 6 || b == 9 || b == 11 && a == 30) {
+        ngay = 1;
+        thang = b++;
+        nam = c;
+    } 
+    else if (a == 28 || a == 29 && b == 2) {
+        ngay = 1;
+        thang = 3;
+        nam = c;
+    }
+    else {
+        ngay = a + 1;
+        thang = b;
+        nam = c;
+    }
+    result = ngay + "/" + thang + "/" + nam;
+    return result;
+}
+
+function doanHomQua (a, b, c) {
+    var result;
+    var ngay;
+    var thang;
+    var nam;
+    if (a == 1 && b == 1) {
+        ngay = 31;
+        thang = 12;
+        nam = c - 1
+    }
+    else if (a == 1 && b == 2 || b == 4 || b == 6 || b == 9 || b == 11) {
+        ngay = 31;
+        thang = b - 1;
+        nam = c;
+    }
+    else if (a == 1 && b == 5 || b == 7 || b == 8 || b == 10 || b == 12) {
+        ngay = 30;
+        thang = b -1;
+        nam = c;
+    }
+    else if (a == 1 ** b == 3) {
+        ngay = 28;
+        thang = 2;
+        nam = c;
+    }
+    else {
+        ngay = a -1;
+        thang = b;
+        nam = c;
+    }
+    result = ngay + "/" + thang + "/" + nam;
+    return result;
+}
+
+function tinhNgayMai () {
+    var result = document.getElementById("result__5");
+    var ngay = document.getElementById("ngay").value * 1;
+    var thang = document.getElementById("thang").value * 1;
+    var nam = document.getElementById("nam").value * 1;
+    if (ngay <= 0 || thang <= 0 || nam <= 0) {
+        result.innerHTML = "Điền ngày tháng năm hợp lý";
+    }
+    else {
+        var ketQua = doanNgayMai(ngay, thang, nam);
+        result.innerHTML = ketQua;
+    }
+}
+
+function tinhHomQua () {
+    var result = document.getElementById("result__5");
+    var ngay = document.getElementById("ngay").value * 1;
+    var thang = document.getElementById("thang").value * 1;
+    var nam = document.getElementById("nam").value * 1;
+    if (ngay <= 0 || thang <= 0 || nam <= 0) {
+        result.innerHTML = "Điền ngày tháng năm hợp lý";
+    }
+    else {
+        var ketQua = doanHomQua(ngay, thang, nam);
+        result.innerHTML = ketQua;
+    }
+}
+
+// ĐẾM NGÀY
+function tinhSoNgay(a, b) {
+    var ngay;
+    if (a == 1 || a == 3 || a == 5 || a == 7 || a == 8 || a == 10 || a == 12) {
+        ngay = 31;
+    }
+    else {
+        if (a == 2 && b%4 === 0) {
+            ngay = 29;
+        }
+        else if (a == 2 && b%4 !== 0) {
+            ngay = 28;
+        }
+        else {
+            ngay = 30;
+        }
+    }
+    return ngay;
+}
+
+function demNgay() {
+    var result = document.getElementById("result__6");
+    var thang = document.getElementById("demThang").value * 1;
+    var nam = document.getElementById("demNam").value * 1;
+    if (thang <= 0 || nam <= 0) {
+        result.innerHTML = "Điền ngày tháng năm hợp lý";
+    }
+    else {
+        var ketQua = tinhSoNgay(thang, nam);
+        result.innerHTML = "Tháng " + thang + " năm  " + nam + " có " + ketQua + " ngày.";
+    }
+}
+
+//ĐỌC SỐ
+function phanLoai(a) {
+    var result;
+    if (a == 1) {
+        result = "một";
+    }
+    else if (a == 2) {
+        result = "hai";
+    }
+    else if (a == 3) {
+        result = "ba";
+    }
+    else if (a == 4) {
+        result = "bốn";
+    }
+    else if (a == 5) {
+        result = "năm";
+    }
+    else if (a == 6) {
+        result = "sáu";
+    }
+    else if (a == 7) {
+        result = "bảy";
+    }
+    else if (a == 8) {
+        result = "hai";
+    }
+    else if (a == 9) {
+        result = "chín";
+    }
+    return result;
+}
+
+function cachDoc(a) {
+    var hangTram = parseInt(a/100);
+    var hangChuc = parseInt((a - hangTram*100)/10);
+    var hangDonVi = parseInt(a - hangChuc*10 - hangTram*100);
+    var result;
+    if (hangChuc === 0) {
+        if(hangDonVi === 0) {
+            result = phanLoai(hangTram) + " trăm";
+        }
+        else {
+            result = phanLoai(hangTram) + " trăm lẻ " + phanLoai(hangDonVi);
+        }
+    }
+    else if (hangChuc == 1) {
+        if(hangDonVi === 0) {
+            result = phanLoai(hangTram) + " trăm mười" ;
+        }
+        else {
+            result = phanLoai(hangTram) + " trăm mười " + phanLoai(hangDonVi);
+        }
+    }
+    else if(hangDonVi === 0) {
+        result = phanLoai(hangTram) + " trăm " + phanLoai(hangChuc) + " mươi ";
+    }
+    else {
+        if(hangDonVi == 1) {
+            result = phanLoai(hangTram) + " trăm " + phanLoai(hangChuc) + " mươi mốt";
+        }
+        else {
+            result = phanLoai(hangTram) + " trăm " + phanLoai(hangChuc) + " mươi " + phanLoai(hangDonVi);
+        }
+    }
+    return result;
+}
+
+function docSo() {
+    var result = document.getElementById("result__7");
+    var so = document.getElementById("nhapSo").value * 1;
+    if (so.toString().length !== 3) {
+        result.innerHTML = "Chỉ nhập số có 3 chữ số";
+    }
+    else {
+        var ketQua = cachDoc(so);
+        result.innerHTML = ketQua;
+    }
 }
